9c8d162d96381996501b5e5803ae718373acaff8
Robert Lewandowski <rrb.lewandowski@gmail.com>
2018-04-09 01:17:04 +0200

    Board and Snake

--- Snake/Snake/MainWindow.xaml.cs
+++ Snake/Snake/MainWindow.xaml.cs
@@ -20,9 +20,45 @@ namespace Snake
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly int SIZE = 10;
+        private MySnake _snake;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitBoard();
+            InitSnake();
         }
+
+        void InitBoard()
+        {
+            for (int i = 0; i < grid.Width / SIZE; i++)
+            {
+                ColumnDefinition columnDefinition = new ColumnDefinition();
+                columnDefinition.Width = new GridLength(SIZE);
+                grid.ColumnDefinitions.Add(columnDefinition);
+            }
+
+            for(int i = 0; i < grid.Height; i++)
+            {
+                RowDefinition rowDefinition = new RowDefinition();
+                rowDefinition.Height = new GridLength(SIZE);
+                grid.RowDefinitions.Add(rowDefinition);
+            }
+
+            _snake = new MySnake();
+        }
+
+        void InitSnake()
+        {
+            grid.Children.Add(_snake.Head.Rect);
+            
+            foreach (SnakePart snakePart in _snake.Parts)
+            {
+                grid.Children.Add(snakePart.Rect);
+                _snake.RedrawSnake();
+            }
+        }
+
     }
 }
